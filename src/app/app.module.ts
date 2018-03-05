2a45f4cedcdbcf68901d27f5ae488a1323a4c6c3
Ian Stewart <iacostewart@msn.com>
2018-03-04 20:06:44 -0500

    created routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { Routes, RouterModule } from '@angular/router';
 
+
+const routes: Routes = [
+  {path: '',
+  component: HomeComponent
+},
+{
+  path: 'register',
+  component: RegisterComponent
+},
+{
+  path:'**', redirectTo: '/'
+}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
